a428483c30166e49a1cfd6e0ea59d0b0bbdf6f98
BryanGordon <bryanbge92@gmail.com>
2024-05-10 14:48:56 -0500

    Change file extension

---
+++ src/icons/Pause.jsx
@@ -0,0 +1,18 @@
+export function Pause() {
+  return (
+    <svg  
+      width="24"  
+      height="24"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="#050505"  
+      stroke-width="2"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M6 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+      <path d="M14 5m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+    </svg>
+  )
+}
