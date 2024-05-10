07a85302b092a06be2998e9808ab9b0077e75dab
BryanGordon <bryanbge92@gmail.com>
2024-05-10 14:49:15 -0500

    Add new icons

---
+++ src/icons/PlayerPrev.jsx
@@ -0,0 +1,18 @@
+export function PlayerPrev () {
+  return (
+    <svg  
+      width="24"  
+      height="24"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="#050505"  
+      stroke-width="2"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M21 5v14l-8 -7z" />
+      <path d="M10 5v14l-8 -7z" />
+    </svg>
+  )
+}
