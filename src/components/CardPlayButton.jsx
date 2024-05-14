5d8a9afcbf0d77e17101d8286d2287769018a288
BryanGordon <bryanbge92@gmail.com>
2024-05-13 19:40:15 -0500

    Create play button play

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,30 @@
+import { Pause } from '#@/icons/Pause.jsx';
+import { Play } from '#@/icons/Play.jsx';
+import { usePlayerStore } from '#@/store/playerStore.js';
+
+export function CardPlayButton ({ id }) {
+  const { currentMusic, isPlaying, setCurrentMusic, setIsPlaying } = usePlayerStore(state => state)
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+  
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false)
+      return
+    }
+
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data
+        setIsPlaying(true)
+        setCurrentMusic({ songs, playlist, song: songs[0] })
+
+      })
+  }
+
+  return (
+    <button onClick={handleClick} className='card-play-button rounded-full bg-green-500 p-2'>
+      { isPlayingPlaylist ? <Pause /> : <Play /> }
+    </button>
+  )
+}
