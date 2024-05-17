e6744c46d586729ddc7db0ded639c46aa0c114f5
BryanGordon <bryanbge92@gmail.com>
2024-05-16 23:09:13 -0500

    Add props

--- src/icons/Play.jsx
+++ src/icons/Play.jsx
@@ -1,6 +1,7 @@
-export function Play () {
+export function Play ({ className }) {
   return (
-    <svg  
+    <svg
+      className={className}  
       width="24"  
       height="24"  
       viewBox="0 0 24 24"  
